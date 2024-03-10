cd383041bdccdab591cb8f0b566b6c0b9b537087
ali <alimuhammad7465@gmail.com>
2024-03-10 13:07:59 +0500

    unaryexpr

--- Assignment2/ActivationFunctions_device.h
+++ Assignment2/ActivationFunctions_device.h
@@ -3,7 +3,8 @@
 #pragma once
 #include <cmath>
 
-using namespace std;
+namespace Activation{
+
 
 __device__ double sigmoid(double x)
 {
@@ -51,6 +52,7 @@ __device__ double relu_prime(double x)
 __device__ double one_minus(double x)
 {
 	return 1 - x;
+}
 }
 
 #endif
